60a3f447c1f6a8a74ce92b612435ff20394b2a11
Jaime Rueda <rueda10@Jaimes-MacBook-Pro.local>
2017-06-18 15:53:44 -0500

    Removed force:true from sequelize sync

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set("view engine", "handlebars");
 
 app.use("/", routes);
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
   });
